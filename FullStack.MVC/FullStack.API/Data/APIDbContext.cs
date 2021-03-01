6c22d0a39b4de81714bf1230ea5f4df2f5feab0c
Alejandro de Arriba <adearriba@outlook.com>
2021-03-01 23:01:29 +0100

    Created DBConext
    Added Models for tables

---
+++ FullStack.MVC/FullStack.API/Data/APIDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FullStack.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Mobile> Mobiles { get; set; }
+
+        public APIDbContext(DbContextOptions<APIDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
