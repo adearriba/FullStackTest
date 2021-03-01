78200697bd1f6f5cee8293ad8956ec6f2f651601
Alejandro de Arriba <adearriba@outlook.com>
2021-03-01 21:16:45 +0100

    Init proyecto API

---
+++ FullStack.MVC/FullStack.API/Controllers/WeatherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FullStack.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class WeatherController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
