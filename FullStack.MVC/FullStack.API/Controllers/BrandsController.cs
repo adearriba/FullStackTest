f37dace9d68c43368927811336c1c41138e7b212
Alejandro de Arriba <adearriba@outlook.com>
2021-03-01 23:18:41 +0100

    Mobile and brand controllers

--- FullStack.MVC/FullStack.API/Controllers/BrandsController.cs
+++ FullStack.MVC/FullStack.API/Controllers/BrandsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FullStack.API.Model;
+using FullStack.API.Repositories;
+using FullStack.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace FullStack.API.Controllers
 {
@@ -7,10 +12,44 @@ namespace FullStack.API.Controllers
     [ApiController]
     public class BrandsController : ControllerBase
     {
-        [HttpGet]
-        [ProducesResponseType((int)HttpStatusCode.OK)]
-        public IActionResult Index()
+        private readonly IBrandRepository _repository;
+
+        public BrandsController(IBrandRepository repository)
         {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Brand>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public IActionResult GetAll()
+        {
+            var brands = _repository.GetAll();
+            return brands.Count > 0 ? Ok(brands) : NotFound();
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Brand), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var brand = await _repository.GetAsync(id);
+            return brand != null ? Ok(brand) : NotFound();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Brand), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Add(Brand brand)
+        {
+            var result = await _repository.AddAsync(brand);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Brand), (int)HttpStatusCode.OK)]
+        public IActionResult Update(Brand brand)
+        {
+            _repository.Update(brand);
             return Ok();
         }
     }
--- FullStack.MVC/FullStack.API/Controllers/MobileController.cs
+++ FullStack.MVC/FullStack.API/Controllers/MobileController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FullStack.API.Model;
+using FullStack.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace FullStack.API.Controllers
 {
@@ -7,10 +11,44 @@ namespace FullStack.API.Controllers
     [ApiController]
     public class MobileController : Controller
     {
-        [HttpGet]
-        [ProducesResponseType((int)HttpStatusCode.OK)]
-        public IActionResult Index()
+        private readonly IMobileRepository _repository;
+
+        public MobileController(IMobileRepository repository)
         {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Mobile>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public IActionResult GetAll()
+        {
+            var mobiles = _repository.GetAll();
+            return mobiles.Count > 0 ? Ok(mobiles) : NotFound();
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Brand), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var mobile = await _repository.GetAsync(id);
+            return mobile != null ? Ok(mobile) : NotFound();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Brand), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Add(Mobile mobile)
+        {
+            var result = await _repository.AddAsync(mobile);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Brand), (int)HttpStatusCode.OK)]
+        public IActionResult Update(Mobile mobile)
+        {
+            _repository.Update(mobile);
             return Ok();
         }
     }
