c50cb761a70f70a195b0a9beb02c17be0211d7df
Alejandro de Arriba <adearriba@outlook.com>
2021-03-02 15:04:07 +0100

    Basic MVC controllers

---
+++ FullStack.MVC/FullStack.MVC/Controllers/BrandsController.cs
@@ -0,0 +1,105 @@
+﻿using FullStack.Models;
+using FullStack.MVC.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace FullStack.MVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        // GET: BrandsController
+        public async Task<ActionResult> Index()
+        {
+            var brands = await _brandService.GetAllAsync();
+            return View(brands);
+        }
+
+        // GET: BrandsController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var brand = await _brandService.GetByIdAsync(id);
+            return View(brand);
+        }
+
+        // GET: BrandsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([FromForm] Brand brandInput)
+        {
+            try
+            {
+                var brand = await _brandService.AddAsync(brandInput);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var brand = await _brandService.GetByIdAsync(id);
+            return View(brand);
+        }
+
+        // POST: BrandsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, [FromForm] Brand brandInput)
+        {
+            try
+            {
+                await _brandService.UpdateAsync(brandInput);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                var brand = await _brandService.GetByIdAsync(id);
+                return View(brand);
+            }
+        }
+
+        // GET: BrandsController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var brand = await _brandService.GetByIdAsync(id);
+            return View(brand);
+        }
+
+        // POST: BrandsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                await _brandService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                var brand = await _brandService.GetByIdAsync(id);
+                return View(brand);
+            }
+        }
+    }
+}
---
+++ FullStack.MVC/FullStack.MVC/Controllers/MobileController.cs
@@ -0,0 +1,104 @@
+﻿using FullStack.Models;
+using FullStack.MVC.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FullStack.MVC.Controllers
+{
+    [Authorize(Roles = "Admin,Operator")]
+    public class MobileController : Controller
+    {
+        private readonly IMobileService _mobileService;
+
+        public MobileController(IMobileService mobileService)
+        {
+            _mobileService = mobileService;
+        }
+
+        // GET: MobileController
+        public async Task<ActionResult> Index()
+        {
+            var mobiles = await _mobileService.GetAllAsync();
+            return View(mobiles);
+        }
+
+        // GET: MobileController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var mobile = await _mobileService.GetByIdAsync(id);
+            return View(mobile);
+        }
+
+        // GET: MobileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MobileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([FromForm] Mobile mobileInput)
+        {
+            try
+            {
+                var mobile = await _mobileService.AddAsync(mobileInput);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MobileController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var mobile = await _mobileService.GetByIdAsync(id);
+            return View(mobile);
+        }
+
+        // POST: MobileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, [FromForm] Mobile mobileInput)
+        {
+            try
+            {
+                await _mobileService.UpdateAsync(mobileInput);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                var mobile = await _mobileService.GetByIdAsync(id);
+                return View(mobile);
+            }
+        }
+
+        // GET: MobileController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var mobile = await _mobileService.GetByIdAsync(id);
+            return View(mobile);
+        }
+
+        // POST: MobileController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                await _mobileService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                var mobile = await _mobileService.GetByIdAsync(id);
+                return View(mobile);
+            }
+        }
+    }
+}
