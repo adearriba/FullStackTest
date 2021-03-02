bfec81f30b46ac98643b92b2b24f32074d23297d
Alejandro de Arriba <adearriba@outlook.com>
2021-03-02 11:26:28 +0100

    Initial Identity Server migrations

--- FullStack.MVC/FullStack.MVC/Data/Migrations/20210301212927_InitialMigration.Designer.cs
+++ FullStack.MVC/FullStack.MVC/Data/Migrations/20210301212927_InitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using FullStack.MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FullStack.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210301212927_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
