04b6cbdaa65da06c46de811f413ed30725ccea61
Alejandro de Arriba <adearriba@outlook.com>
2021-03-03 15:34:58 +0100

    Mobile frontend changes

--- FullStack.MVC/FullStack.MVC/Models/Mobile/CreateMobileInputModel.cs
+++ FullStack.MVC/FullStack.MVC/Models/Mobile/CreateMobileInputModel.cs
@@ -11,8 +11,26 @@ namespace FullStack.MVC.Models.Mobile
     {
         [Display(Name = "Modelo")]
         public string Model { get; set; }
+
         [Display(Name = "Marca")]
         public int BrandId { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+
+        [Display(Name = "Cámara")]
+        public string CamaraDescripcion { get; set; }
+
+        [Display(Name = "Almacenamiento")]
+        public string StorageDescription { get; set; }
+
+        [Display(Name = "Pantalla")]
+        public string ScreenDescription { get; set; }
+
+        [Display(Name = "Batería")]
+        public string BateryDescription { get; set; }
+
+
         public List<Brand> Brands { get; set; }
 }
 }
