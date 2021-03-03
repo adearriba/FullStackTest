7ecca24967cb86c567ec938926a55fb7dff0b2f8
Alejandro de Arriba <adearriba@outlook.com>
2021-03-03 17:09:19 +0100

    Scaffolding of Identity pages

---
+++ FullStack.MVC/FullStack.MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FullStack.MVC.Data;
+using FullStack.MVC.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FullStack.MVC.Areas.Identity.IdentityHostingStartup))]
+namespace FullStack.MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
