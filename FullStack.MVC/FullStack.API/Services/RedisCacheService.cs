de3a2b0d18e3309fe894ff46da83c4bfdd7e4c5b
Alejandro de Arriba <adearriba@outlook.com>
2021-03-04 09:05:50 +0100

    Better failure resistance for Redis Cache

--- FullStack.MVC/FullStack.API/Services/RedisCacheService.cs
+++ FullStack.MVC/FullStack.API/Services/RedisCacheService.cs
@@ -1,6 +1,8 @@
 ﻿using FullStack.API.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FullStack.API.Services
@@ -8,22 +10,41 @@ namespace FullStack.API.Services
     public class RedisCacheService : ICacheService
     {
         private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private readonly ILogger<RedisCacheService> _logger;
 
-        public RedisCacheService(IConnectionMultiplexer connectionMultiplexer)
+        public RedisCacheService(
+            IConnectionMultiplexer connectionMultiplexer, 
+            ILogger<RedisCacheService> logger)
         {
             _connectionMultiplexer = connectionMultiplexer;
+            _logger = logger;
         }
 
         public async Task<string> GetValueAsync(string key)
         {
-            var db = _connectionMultiplexer.GetDatabase();
-            return await db.StringGetAsync(key);
+            try
+            {
+                var db = _connectionMultiplexer.GetDatabase();
+                return await db.StringGetAsync(key);
+            }
+            catch (RedisConnectionException redisEx)
+            {
+                _logger.LogError(redisEx, "Connection problem with Redis Service.");
+                return null;
+            }
         }
 
         public async Task SetValueAsync(string key, string value)
         {
-            var db = _connectionMultiplexer.GetDatabase();
-            await db.StringSetAsync(key, value, TimeSpan.FromSeconds(60));
+            try
+            {
+                var db = _connectionMultiplexer.GetDatabase();
+                await db.StringSetAsync(key, value, TimeSpan.FromSeconds(60));
+            }
+            catch(RedisConnectionException redisEx)
+            {
+                _logger.LogError(redisEx, "Connection problem with Redis Service.");
+            }
         }
     }
 }
